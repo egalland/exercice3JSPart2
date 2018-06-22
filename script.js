a6cbd6dcd6a449ec5dc91bd4f78b02c158f205d2
egalland <emmanuel.galland117@gmail.com>
2018-06-22 08:49:18 +0200

    maj commentaires

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+// FONCTION LORSQU'ON TAPE UNE TOUCHE DANS LE CHAMPS DE TEXTE
 function onKeyPress(){
+  // DECLARATION ET RECUPERATION DES VARIABLES
     var lastname = document.getElementById('lastname').value;
+    // BOITE DE DIALOGUE AVEC LA TOUCHE TAPEE
     alert(lastname);
 }
